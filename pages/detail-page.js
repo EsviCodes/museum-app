d2dde4bc0218f4f828e1e55ecd85d0933de4bd7c
Esther Vis <Esther@Valeries-MBP.localdomain>
2019-09-20 12:26:32 +0200

    Set up js files and declare submitComment

---
+++ pages/detail-page.js
@@ -0,0 +1,17 @@
+// Create an input field for the name, one for the comment and an submit-button
+// Done in HTML
+
+// Add an event (name and comment) listener/handler to the button
+function submitComment() {
+  console.log("Hello");
+}
+
+// Gather data of the input fields
+
+// Create some elements (to display the data)
+
+// Adjust the elements so they display the data
+
+// Display the adjusted elements on your page
+
+// Empty the input fields so the next user can type some new text.
